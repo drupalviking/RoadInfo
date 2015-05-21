9804e8108692f28bf79a6774448b2c4b9b1b1e91
Hilmar Hallbjornsson <drupalviking@gmail.com>
2015-05-21 09:53:00 +0000

    Initial commit.  Skeleton

---
+++ config/module.config.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+return array(
+	'session' => array(
+		'remember_me_seconds' => 2419200,
+		'use_cookies' => true,
+		'cookie_httponly' => true,
+	),
+    'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'RoadInfo\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ),
+        'aliases' => array(
+            'translator' => 'MvcTranslator',
+        ),
+    ),
+    'translator' => array(
+        'locale' => 'en_US',
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'RoadInfo\Controller\Index' => 'RoadInfo\Controller\IndexController',
+        ),
+    ),
+    'view_helpers' => array(
+        'invokables' => array(
+            'paragrapher' => 'RoadInfo\View\Helper\Paragrapher',
+        ),
+    ),
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+		'base_path' => '/roadinfo/',
+        'strategies' => array(
+            'ViewFeedStrategy',
+			'ViewJsonStrategy',
+        ),
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+			'layout/landing'           => __DIR__ . '/../view/layout/landing.phtml',
+			'layout/anonymous'           => __DIR__ . '/../view/layout/anonymous.phtml',
+			'layout/csv'           	  => __DIR__ . '/../view/layout/csv.phtml',
+            'roadinfo/index/index' => __DIR__ . '/../view/roadinfo/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+			'error/401'               => __DIR__ . '/../view/error/401.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    // Placeholder for console routes
+    'console' => array(
+        'router' => array(),
+    ),
+);
